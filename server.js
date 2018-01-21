d037992c81501ee9d709a65bdd356749cfc0db75
erik_dussel <erikdussel@gmail.com>
2018-01-21 23:13:55 +0100

    update path

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var io = require('socket.io').listen(server);
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/css', express.static(__dirname + '/css'));
+app.use('/cards', express.static(__dirname + '/cards'));
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/index.html');
 })
